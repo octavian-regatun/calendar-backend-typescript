509e238c815b857df4b6ee93dbe3f65d80089ff1
Octavian Regatun <octavian.regatun@outlook.com>
2021-04-15 00:12:39 +0300

    .

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
 import dotenv from 'dotenv';
 dotenv.config();
-import express, { NextFunction, Request, Response } from 'express';
+import express from 'express';
 import cors from 'cors';
 import config from './config';
 import { routes } from './routes/routes';
-import path from 'path';
 
 (async (): Promise<void> => {
   await import('./database');
@@ -16,20 +15,7 @@ import path from 'path';
   app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
 
-  app.use('/api', routes);
-
-  if (process.env.NODE_ENV === 'production') {
-    app.use(
-      express.static(path.join(__dirname, process.env.CLIENT_PATH || '')),
-    );
-
-    // Handle React routing, return all requests to React app
-    app.get('*', (req, res) => {
-      res.sendFile(
-        path.join(__dirname, process.env.CLIENT_PATH || '', 'index.html'),
-      );
-    });
-  }
+  app.use('/', routes);
 
   app.listen(config.PORT, () => {
     console.log(`Server running on port: ${config.PORT}`);
